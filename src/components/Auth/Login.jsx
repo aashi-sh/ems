e0a5cae0a1ecf3c2acae9b1fd2809aa6aa67e12a
aashish pandit <aashishpandita11@gmail.com>
2025-03-06 15:47:46 +0530

    First Commit

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "./Login.css";
+import { useState } from "react";
+
+
+const Login = () =>{
+
+    const[email, setEmail] = useState("")
+
+    const[password, setPassword] = useState("")
+
+    const submitHandler = (e) =>{
+        e.preventDefault()
+        console.log("email is", email)
+        console.log('password is', password)
+
+        setEmail("")
+        setPassword("")
+        
+    }
+
+
+
+    return (
+        <div className="LoginContainer">
+            <div className="OuterContainer">
+                <form 
+                className="InnerLoginContainer"
+                onSubmit ={(e)=>{
+                    submitHandler(e)}}
+                >
+
+                    <input
+                    value={email}
+                    onChange={(e)=>{setEmail(e.target.value)}}
+                    className="EmailContainer"
+                    type="email" 
+                    placeholder="Enter the Email"/>
+                    
+                    
+                    <input
+                    value={password}
+                    onChange={(e)=>{setPassword(e.target.value)}} 
+                    maxLength={8}
+                    className="PasswordContainer"
+                    type="password" 
+                    placeholder="Enter the Password"/>
+
+                    <button 
+                    className="SubmitContainer">
+                        Login
+                    </button>
+
+
+                </form>
+
+
+
+            </div>
+
+        </div>
+    )
+
+}
+
+export default Login;
